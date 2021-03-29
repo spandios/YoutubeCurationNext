f09cdc7c96891c7d4b3326e146bee9787edd7186
dev_heo <wndudpower@gmail.com>
2021-03-29 23:38:34 +0900

    List padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         </Link>
         <iframe
           width="100%"
-          height="315"
+          height="400"
           src="https://www.youtube.com/embed/xM0RGpnl6MU"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
